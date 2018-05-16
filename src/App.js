124cbdec81adadc5b3da2bcf16efbd2b4e8b8e4c
Rafael Montero <rmontero@Rafaels-MacBook-Air.local>
2018-05-16 14:48:34 -0400

    tadding new files librarylist

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './reducers';
-
+import { Header } from './components/common';
 
 const App = () => {
 
     return(
         <Provider store = {createStore(reducers)}>
             <View>
+                <Header headerText = "Tech stack"/>
                 <Text> This is my tech stack app</Text>
-                </View>
+            </View>
         </Provider>
     );
 
---
+++ src/reducers/LibraryReducer.js
@@ -0,0 +1,3 @@
+import data from './LibraryReducer.js';
+
+export default () => data;
